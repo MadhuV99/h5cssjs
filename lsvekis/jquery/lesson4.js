494006bd55b6c4b0c2bbb5ed6124601c39733c61
Madhu Vasudevan <madhuvasudevan@yahoo.com>
2021-04-02 21:43:46 -0500

    Latest Changes

---
+++ lsvekis/jquery/lesson4.js
@@ -0,0 +1,6 @@
+$(".red").css({
+    color : "red",
+    border: "1px solid blue",
+    fontSize: "26px",
+    backgroundColor: "yellow"
+});
---
+++ lsvekis/jquery/lesson5.js
@@ -0,0 +1,5 @@
+
+//$("li").text("Hello<br>World");
+//$("li").html("Hello<br>World");
+//$("li").append("<div>APPENDED</div> ");
+$("li").prepend("PREPEND");
