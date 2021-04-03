5ba5e2fc96f06816af0d26d8cd4fcc6caaed35bc
Madhu Vasudevan <madhuvasudevan@yahoo.com>
2021-04-02 22:32:14 -0500

    Latest Changes

---
+++ lsvekis/jquery/lesson9.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    console.log("loaded and ready to go");
+    $("a").click(function(e){
+        e.preventDefault();
+        $(this).css("color","red");
+    })
+    $("h1").hover(function(){
+        $(this).toggleClass("red");
+    })
+    /*
+    $("h1").click(function(){
+        $(this).css("backgroundColor","white");
+    })
+    */
+})
