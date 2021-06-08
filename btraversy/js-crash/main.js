39eea9186e0bced69471fd7ddeee92f112bcf72c
Madhu Vasudevan <madhuvasudevan@yahoo.com>
2021-06-07 21:15:30 -0500

    latest Changes

---
+++ btraversy/js-crash/main.js
@@ -0,0 +1,27 @@
+// alert('Hello World');
+// console.log('Hello World');
+// console.error('This is an error!');
+// console.warn('This is a warning!');
+
+//var, let, const
+// let age = 30;
+// const age = 30;
+// age = 31;
+// console.log(age);
+
+const aname = 'John';
+const age = 30;
+const rating = 4.5;
+const isCool = true;
+const x = null;
+const y = undefined;
+let z;
+
+console.log(typeof aname)
+console.log(typeof age)
+console.log(typeof rating)
+console.log(typeof isCool)
+console.log(typeof x)
+console.log(typeof y)
+console.log(typeof z)
+
